b3de5edd244d626491ea78dddc4870b8cc4e73d5
EmersonPL <emersonphilopes@gmail.com>
2022-03-22 21:48:56 -0300

    Delete src/tests directory

--- src/tests/pointSpec.js
+++
@@ -1,8 +0,0 @@
-describe('Testing Point creation', () => {
-    it('Should have the correct Positions', () => {
-            let p1 = new Point(10, 10, '#000000');
-
-            expect(p1.getPosition()).toEqual([10, 10]);
-        }
-    )
-})
